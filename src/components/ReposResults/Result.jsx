e0d7b5cf64d4533757bc6dfb34282361ae0efbb0
Julien <newdevcreator@gmail.com>
2024-04-07 17:54:13 +0200

    FEAT: card

--- src/components/ReposResults/Result.jsx
+++ src/components/ReposResults/Result.jsx
@@ -5,7 +5,7 @@ import Repo from "./Repo";
 export default function Result({ results }) {
   return (
     <Card className="mt-4" radius="none" shadow="none">
-      <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+      <div className="gap-2 grid grid-cols-2 sm:grid-cols-3">
         {results.map((item) => (
           <Repo key={item.id} {...item} />
         ))}
