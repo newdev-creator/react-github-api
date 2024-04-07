21cbc87dd592e909bbbe504ad7e5a7660ffb94d7
Julien <newdevcreator@gmail.com>
2024-04-08 00:05:56 +0200

    FEAT: add external link

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # react-github-api
+
+Simply search in the Github repository.
+
+This App use :
+
+- React
+- TailwindCSS
+- NextUi
+
+# Link
+
+<https://react-note-app-beta-ruddy.vercel.app/>
--- src/components/ReposResults/Repo.jsx
+++ src/components/ReposResults/Repo.jsx
@@ -1,16 +1,19 @@
 import PropTypes from "prop-types";
-import { Card, CardBody, Image, CardHeader } from "@nextui-org/react";
+import { Card, CardBody, Image, CardHeader, Link } from "@nextui-org/react";
 
-export default function Repo({ name, description, owner }) {
+export default function Repo({ name, description, owner, html_url }) {
   return (
     <Card className="p-2">
-    <Image src={owner.avatar_url} />
-    <CardHeader className="flex flex-col">
-    <h4 className="font-bold text-large">{name}</h4>
-    <p className="text-slate-600 text-xs">{owner.login}</p>
-    </CardHeader>
+      <Image src={owner.avatar_url} />
+      <CardHeader className="flex flex-col">
+        <h4 className="font-bold text-large">{name}</h4>
+        <p className="text-slate-600 text-xs">{owner.login}</p>
+      </CardHeader>
       <CardBody>
-        <p>{description}</p>
+        <p className="mb-4">{description}</p>
+        <Link isExternal href={html_url} showAnchorIcon>
+          External Link
+        </Link>
       </CardBody>
     </Card>
   );
@@ -19,6 +22,7 @@ export default function Repo({ name, description, owner }) {
 Repo.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string,
+  html_url: PropTypes.string,
   owner: PropTypes.shape({
     login: PropTypes.string.isRequired,
     avatar_url: PropTypes.string.isRequired,
