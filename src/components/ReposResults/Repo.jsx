d07b6106432291f811d6db8c26c3762f7e31c238
Julien <newdevcreator@gmail.com>
2024-04-07 17:54:02 +0200

    FEAT: card

--- src/components/ReposResults/Repo.jsx
+++ src/components/ReposResults/Repo.jsx
@@ -1,13 +1,15 @@
 import PropTypes from "prop-types";
-import { Card, CardBody } from "@nextui-org/react";
+import { Card, CardBody, Image, CardHeader } from "@nextui-org/react";
 
 export default function Repo({ name, description, owner }) {
   return (
-    <Card>
+    <Card className="p-2">
+    <Image src={owner.avatar_url} />
+    <CardHeader className="flex flex-col">
+    <h4 className="font-bold text-large">{name}</h4>
+    <p className="text-slate-600 text-xs">{owner.login}</p>
+    </CardHeader>
       <CardBody>
-        <p>{owner.avatar_url}</p>
-        <p>{name}</p>
-        <p>{owner.login}</p>
         <p>{description}</p>
       </CardBody>
     </Card>
